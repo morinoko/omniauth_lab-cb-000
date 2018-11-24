bd3dc02008c654707e3db740bcfa4dd5110e9ea3
Felice Forby <feliceforby@gmail.com>
2018-11-24 01:06:33 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def create
     binding.pry
     @user = User.find_or_create_by(uid: facebook_auth['uid']) do |user|
-      user.name = auth['info']['name']
-      user.email = auth['info']['email']
-      user.image = auth['info']['image']
+      user.name = facebook_auth['info']['name']
+      user.email = facebook_auth['info']['email']
+      user.image = facebook_auth['info']['image']
     end
 
     session[:user_id] = @user.id
