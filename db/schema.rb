566bab88358a1fdb618fd66f2e0ffa6aaf8ff9d8
Felice Forby <feliceforby@gmail.com>
2018-11-24 01:08:10 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181124001829) do
-
-  create_table "users", force: :cascade do |t|
-    t.string  "name"
-    t.string  "email"
-    t.string  "image"
-    t.integer "uid"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
