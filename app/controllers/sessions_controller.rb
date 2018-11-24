d6dabdc6e95ca8a6ad1f09fcfb91396bb7d98d8e
Felice Forby <feliceforby@gmail.com>
2018-11-24 01:22:54 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def create
+    @auth = facebook_auth
+
+    @user = User.find_or_create_by(uid: @auth['uid']) do |user|
+      user.name = @auth['info']['name']
+      user.email = @auth['info']['email']
+      user.image = @auth['info']['image']
+    end
+    
+    session[:user_id] = @user.id
+
+    render :create
+  end
+
+  private
+
+  def facebook_auth
+    request.env['omniauth.auth']
+  end
+end
